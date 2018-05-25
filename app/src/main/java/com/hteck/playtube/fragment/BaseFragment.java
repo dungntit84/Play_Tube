30630804812d20e7a331c96366fb582f39fca1f7
Dzung Nguyen <dungntit@hotmail.com>
2018-05-25 17:43:09 +0700

    for testing

--- app/src/main/java/com/hteck/playtube/fragment/BaseFragment.java
+++ app/src/main/java/com/hteck/playtube/fragment/BaseFragment.java
@@ -1,8 +1,12 @@
 package com.hteck.playtube.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.hteck.playtube.activity.MainActivity;
 import com.hteck.playtube.common.Constants;
 
 public abstract class BaseFragment extends Fragment {
@@ -18,5 +22,12 @@ public abstract class BaseFragment extends Fragment {
 
     public View.OnClickListener getGetRightEventListener() {
         return null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        MainActivity.getInstance().setHeader();
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
